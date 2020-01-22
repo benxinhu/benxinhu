3d9d230f7f3dc6cd49e3a2683b142570ae615f0c
benxinhu <60122542+benxinhu@users.noreply.github.com>
2020-01-22 23:37:18 +0800

    Update test.java

--- src/boat/test.java
+++ src/boat/test.java
@@ -4,11 +4,18 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DragonBoat boat;
+		Boat boat;
 		boat = new DragonBoat("Shanghai Team");
 		System.out.println(boat);
 		boat= new DragonBoat("Zhejiang Team", "yellow");
 		System.out.println(boat);
+		boat.Start();
+		
+		boat = new LittleBoat("Shanghai Team");
+		System.out.println(boat);
+		boat= new LittleBoat("Zhejiang Team", "green");
+		System.out.println(boat);
+		boat.Start();
 	}	
 
 }
